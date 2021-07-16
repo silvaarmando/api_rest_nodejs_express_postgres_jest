10bf5db17c38dbce3dd7680fc0b34d8c53755e87
franciscoarmando63 <franciscosilva7877@gmail.com>
2021-07-16 16:04:16 -0300

    Initial Part Three

--- server/infra/database.js
+++ server/infra/database.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 
 const db = pgp({
   user: 'postgres',
-  password: '10072304',
+  password: '',
   host: 'localhost',
   port: 5432,
   database: 'apiBlogNodejsExpress'
--- server/route/postsRoute.js
+++ server/route/postsRoute.js
@@ -9,21 +9,4 @@ router.get('/posts', async function (req, res) {
   res.json(posts);
 });
 
-router.get('/posts/:id', async function (req, res) {
-
-});
-
-router.post('/posts', async function (req, res) {
-
-});
-
-router.put('/posts/:id', async function (req, res) {
-
-});
-
-router.delete('/posts/:id', async function (req, res) {
-
-});
-
-
 module.exports = router;
